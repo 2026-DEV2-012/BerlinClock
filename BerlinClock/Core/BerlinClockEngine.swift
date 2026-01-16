7115272147fdb13ac2e67685ba42f456953013d6
2026-DEV2-012 <2026-DEV2-012@gmail.com>
2026-01-16 22:45:28 +0100

    feat: implement five minutes lamp

--- BerlinClock/Core/BerlinClockEngine.swift
+++ BerlinClock/Core/BerlinClockEngine.swift
@@ -6,26 +6,32 @@
 //
 
 struct BerlinClockEngine {
-    init() {}
-    
+    init() { }
+
     func getSecondsLamp(seconds: Int) -> LampState {
         return seconds.isMultiple(of: 2) ? .yellow : .off
     }
-    
+
     func getFiveHoursLamps(hours: Int) -> [LampState] {
         let redLampsCount: Int = hours / 5
         return (0..<4).map { $0 < redLampsCount ? .red : .off }
     }
-    
+
     func getSingleHoursLamps(hours: Int) -> [LampState] {
         let modulo = hours % 5
         return (0..<4).map { $0 < modulo ? .red : .off }
     }
-    
+
     func getFiveMinutesLamps(minutes: Int) -> [LampState] {
-        return (0..<11).map { _ in .red }
+        let litLampsCount: Int = minutes / 5
+        let redPositions = [2, 5, 8]
+
+        return (0..<11).map { value in
+            guard value < litLampsCount else { return .off }
+            return redPositions.contains(value) ? .red : .yellow
+        }
     }
-    
+
     func getSingleMinutesLamps(minutes: Int) -> [LampState] {
         let modulo = minutes % 5
         return (0..<4).map { $0 < modulo ? .yellow : .off }

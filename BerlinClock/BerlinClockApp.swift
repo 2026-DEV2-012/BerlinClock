78cbbfc06a5d5cc85ba86847889060da8fef2208
2026-DEV2-012 <2026-DEV2-012@gmail.com>
2026-01-16 14:36:48 +0100

    chore: init project, gitignore and directory structure

---
+++ BerlinClock/BerlinClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerlinClockApp.swift
+//  BerlinClock
+//
+//  Created by Julien Henrard on 12/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct BerlinClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BerlinClock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BerlinClock
+//
+//  Created by Julien Henrard on 12/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Berlin Clock kata")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
